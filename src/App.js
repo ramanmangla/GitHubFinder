8486489ac983cb0425eadc4828dff12071a6404f
Raman Mangla <ramanmangla7@gmail.com>
2019-07-23 14:20:35 -0400

    Added navbar component to the project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import './App.css';
 
 class App extends Component {
-  foo = () => "Bars";
+  foo = () => 'Bars';
 
   render() {
-    const name = "John Doe";
-    const loading = false;
-
-    // if (loading) {
-    //   return <h4>Loading...</h4>;
-    // }
-
     return (
       // React Fragment over div to use a ghost container
       // Fragment isn't visible in DOM
-      <div className="App">
-        {loading ? (
-          <h4>Loading...</h4>
-        ) : (
-          <h1>
-            Hello {name.toUpperCase()} {this.foo()}
-          </h1>
-        )}
+      <div className='App'>
+        <Navbar />
       </div>
     );
   }
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class UserItem extends Component {
+  render() {
+    return <div>UserItem</div>;
+  }
+}
+
+export default UserItem;
