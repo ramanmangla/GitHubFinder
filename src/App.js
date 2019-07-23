2cc284f656d053983d0a7ee35a915de0fdf7494e
Raman Mangla <ramanmangla7@gmail.com>
2019-07-23 17:29:27 -0400

    Added axios module and get request for fetching GitHub users

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  foo = () => 'Bars';
+  // A Lifecycle method
+  // Runs as soon as the component is mounted
+  componentDidMount() {
+    axios.get('https://api.github.com/users').then(res => console.log(res));
+  }
 
+  // Required Lifecycle method
   render() {
     return (
       // React Fragment over div to use a ghost container
