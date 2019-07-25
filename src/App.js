75bbb136576bed77bcc6223bb119e6e2ce6bdb53
Raman Mangla <ramanmangla7@gmail.com>
2019-07-25 15:54:19 -0400

    Added GitHib search functionality and clear button to clear results

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
-
 // Promise based HTTP client Axios
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
+    // GitHub users list
     users: [],
+    // Loading screen
     loading: false
   };
 
@@ -26,16 +27,39 @@ class App extends Component {
     this.setState({ users: res.data, loading: false });
   }
 
+  // Search GitHub users
+  searchUsers = async text => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=
+        ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${
+      process.env.REACT_APP_GITHUB_CLIENT_SECRET
+    }`);
+
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  // Search GitHub users
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
   // Required Lifecycle method
   render() {
+    const { users, loading } = this.state;
+
     return (
       // React Fragment over div to use a ghost container
       // Fragment isn't visible in DOM
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Search />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Search extends Component {
   state = {
     text: ''
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired
   };
 
   onChange = e => {
@@ -11,10 +17,13 @@ class Search extends Component {
 
   onSubmit = e => {
     e.preventDefault();
-    console.log(this.state.text);
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
   };
 
   render() {
+    const { showClear, clearUsers } = this.props;
+
     return (
       <div>
         <form onSubmit={this.onSubmit} className='form'>
@@ -31,6 +40,12 @@ class Search extends Component {
             className='btn btn-dark btn-block'
           />
         </form>
+
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
